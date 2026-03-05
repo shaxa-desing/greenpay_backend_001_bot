47771ad955cc728b120c37a807dbde3a8040f566
Shahruh <shahruhduschanov@gmail.com>
2026-03-05 17:53:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,5 +58,7 @@ dp.register_message_handler(handlers.payment_phone)
 
 
 if __name__ == "__main__":
-
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     executor.start_polling(dp, skip_updates=True)
+
