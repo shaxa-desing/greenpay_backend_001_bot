7d7b171385b586299648d02fc8a525007882be41
Shahruh <shahruhduschanov@gmail.com>
2026-03-07 22:33:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-from config import BOT_TOKEN
 from handlers import router
+
+BOT_TOKEN = "8565818987:AAEciIAbwHVGjkuJ7TwwdCfKjKlXYj8annI"
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher(storage=MemoryStorage())
+    dp = Dispatcher()
     dp.include_router(router)
     
-    print("Bot muvaffaqiyatli ishga tushdi...")
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
