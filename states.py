964cbc2f453c280ba109d87482cd55b868496718
Shahruh <shahruhduschanov@gmail.com>
2026-03-07 22:32:59 +0500

    Update states.py

--- states.py
+++ states.py
@@ -2,7 +2,7 @@ from aiogram.fsm.state import StatesGroup, State
 
 class UserRegister(StatesGroup):
     name = State()
-    phone = State() # Shu yerda telefon so'rash xatosi tuzatildi
+    phone = State()
 
 class TreePlanting(StatesGroup):
     photo = State()
