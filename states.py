2a7a965a4911d9044b491f63e3fe1eca296302b9
Shahruh <shahruhduschanov@gmail.com>
2026-03-07 17:10:11 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,14 +1,11 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class TreeForm(StatesGroup):
+    registration_phone = State()
     tree_type = State()
     location = State()
-    phone = State()
     photo = State()
-
 
 class PaymentForm(StatesGroup):
     method = State()
-    card = State()
-
-    phone = State()
+    details = State()
