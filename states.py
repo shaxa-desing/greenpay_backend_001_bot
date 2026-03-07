784ca771ba940305ec2d2ac9c2662d6913e725d0
Shahruh <shahruhduschanov@gmail.com>
2026-03-07 19:07:39 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,11 +1,13 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class TreeForm(StatesGroup):
-    registration_phone = State()
     tree_type = State()
     location = State()
     photo = State()
 
 class PaymentForm(StatesGroup):
-    method = State()
     details = State()
+
+# MANA BU QISMNI QO'SHING:
+class UserRegister(StatesGroup):
+    name = State()
