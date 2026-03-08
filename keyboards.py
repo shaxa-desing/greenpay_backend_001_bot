06ffdabf32d4095a896863ba44a78b6d4402fbd1
Shahruh <shahruhduschanov@gmail.com>
2026-03-08 18:36:24 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -9,8 +9,19 @@ def main_menu():
         resize_keyboard=True
     )
 
+
+def main_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="🌳 Daraxt ekish")],
+            [KeyboardButton(text="👤 Shaxsiy kabinet"), KeyboardButton(text="💳 Karta ma'lumotlari")]
+        ],
+        resize_keyboard=True
+    )
+
 def contact_keyboard():
     return ReplyKeyboardMarkup(
         keyboard=[[KeyboardButton(text="📱 Telefon raqamni yuborish", request_contact=True)]],
         resize_keyboard=True, one_time_keyboard=True
     )
+
