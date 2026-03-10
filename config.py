85f95c32bc4518fb42c6255079e521ab6eb57892
Shahruh <shahruhduschanov@gmail.com>
2026-03-10 20:44:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,12 @@
 # MINI_APP_URL = "https://greenpay-webapp.vercel.app/"
 
 # BOT TOKENI (BotFather dan olingan)
-BOT_TOKEN = "8565818987:AAEciIAbwHVGjkuJ7TwwdCfKjKlXYj8annI"
+BOT_TOKEN = "8565818987:AAFtp_uIUnZOdeqLRjWP2E_2eObcEFLJ28o"
 
 # ADMIN ID (O'zingizning Telegram ID raqamingiz)
 ADMIN_ID = 5833828220 
 
 # BACKEND URL
 BACKEND_URL = "https://greenpaybackend-production.up.railway.app"
+
 
